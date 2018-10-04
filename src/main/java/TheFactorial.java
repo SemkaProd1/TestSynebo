ce895a0d90b28b483f663b3f0acfbca44512d2da
SemkaProd1 <semkaprod@gmail.com>
2018-10-05 00:58:18 +0300

    test synebo october: reverse factorial(big) and min squares sides

--- src/main/java/TheFactorial.java
+++ src/main/java/TheFactorial.java
@@ -4,16 +4,24 @@ class TheFactorial {
 
 
     Integer reverseFactorial(Number number) {
-        BigInteger rr = BigInteger.ZERO;
+
+        BigInteger one = BigInteger.ONE;
+
+        BigInteger factorial = one;
+
+        BigInteger rr = one;
+        
         if (number instanceof Long) {
             rr = BigInteger.valueOf((Long) number);
-        } else if (number instanceof Integer) {
+        }
+        if (number instanceof Integer) {
             rr = BigInteger.valueOf((Integer) number);
-        } else if (number instanceof BigInteger) {
+        }
+        if (number instanceof BigInteger) {
             rr = (BigInteger) number;
         }
-        BigInteger factorial = BigInteger.ONE;
-        BigInteger one = BigInteger.ONE;
+
+
         for (BigInteger i = one; rr.compareTo(i) > 0; i = i.add(one)) {
             rr = rr.divide(i);
             factorial = factorial.add(BigInteger.ONE);
